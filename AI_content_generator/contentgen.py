178bd35077bcb4d700b44b62285e4431585f8d86
codegeek03 <shamikbhattacharjee03@kgpian.iitkgp.ac.in>
2025-01-31 01:11:45 +0530

    added

---
+++ AI_content_generator/contentgen.py
@@ -0,0 +1,130 @@
+from crewai_tools import WebsiteSearchTool
+
+from crewai import Agent, Task, Crew
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+llm = ChatGroq(
+    model="mixtral-8x7b-32768",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+web_rag_tool=WebsiteSearchTool()
+
+planner = Agent(
+    llm=llm,
+    role="Content Planner",
+    goal="Plan engaging and factually accurate content on {topic}",
+    backstory="You're working on planning a blog article "
+              "about the topic: {topic}."
+              "You collect information that helps the "
+              "audience learn something "
+              "and make informed decisions. "
+              "Your work is the basis for "
+              "the Content Writer to write an article on this topic.",
+    allow_delegation=False,
+ verbose=True
+)
+
+writer = Agent(
+    llm=llm,
+    role="Content Writer",
+    goal="Write insightful and factually accurate "
+         "opinion piece about the topic: {topic}",
+    backstory="You're working on a writing "
+              "a new opinion piece about the topic: {topic}. "
+              "You base your writing on the work of "
+              "the Content Planner, who provides an outline "
+              "and relevant context about the topic. "
+              "You follow the main objectives and "
+              "direction of the outline, "
+              "as provide by the Content Planner. "
+              "You also provide objective and impartial insights "
+              "and back them up with information "
+              "provide by the Content Planner. "
+              "You acknowledge in your opinion piece "
+              "when your statements are opinions "
+              "as opposed to objective statements.",
+    allow_delegation=False,
+    verbose=True
+)
+editor = Agent(
+    llm=llm,
+    role="Editor",
+    tools=[web_rag_tool],
+    goal="Edit a given blog post to align with "
+         "the writing style of the organization. ",
+    backstory="You are an editor who receives a blog post "
+              "from the Content Writer. "
+              "Your goal is to review the blog post "
+              "to ensure that it follows journalistic best practices,"
+              "provides balanced viewpoints "
+              "when providing opinions or assertions, "
+              "and also avoids major controversial topics "
+              "or opinions when possible.",
+    allow_delegation=False,
+    verbose=True
+)
+
+plan = Task(
+    description=(
+        "1. Prioritize the latest trends, key players, "
+            "and noteworthy news on {topic}.\n"
+        "2. Identify the target audience, considering "
+            "their interests and pain points.\n"
+        "3. Develop a detailed content outline including "
+            "an introduction, key points, and a call to action.\n"
+        "4. Include SEO keywords and relevant data or sources."
+    ),
+    expected_output="A comprehensive content plan document "
+        "with an outline, audience analysis, "
+        "SEO keywords, and resources.",
+    agent=planner,
+)
+
+write = Task(
+    description=(
+        "1. Use the content plan to craft a compelling "
+            "blog post on {topic}.\n"
+        "2. Incorporate SEO keywords naturally.\n"
+  "3. Sections/Subtitles are properly named "
+            "in an engaging manner.\n"
+        "4. Ensure the post is structured with an "
+            "engaging introduction, insightful body, "
+            "and a summarizing conclusion.\n"
+        "5. Proofread for grammatical errors and "
+            "alignment with the brand's voice.\n"
+    ),
+    expected_output="A well-written blog post "
+        "in markdown format, ready for publication, "
+        "each section should have 2 or 3 paragraphs.",
+    agent=writer,
+    output_file="sampleoutput.md"
+)
+
+edit = Task(
+    description=("Proofread the given blog post for "
+                 "grammatical errors and "
+                 "alignment with the brand's voice."),
+    expected_output="A well-written blog post in markdown format, "
+                    "ready for publication, "
+                    "each section should have 2 or 3 paragraphs.",
+    agent=editor
+)
+
+
+
+crew = Crew(
+    agents=[planner, writer, editor],
+    tasks=[plan, write, edit],
+    verbose=2
+)
+
+result = crew.kickoff(inputs={"topic": "Artificial Intelligence?"})
+
+print(result)
