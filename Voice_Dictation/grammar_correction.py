9e82245a544e276352a52cfdebd45cf1e0c53d0e
SRINJOY59 <srinjoydas566@gmail.com>
2025-01-31 02:04:28 +0530

    GPT Caching added

--- Voice_Dictation/grammar_correction.py
+++ Voice_Dictation/grammar_correction.py
@@ -1,5 +1,28 @@
 from langchain_groq import ChatGroq
 
+import hashlib
+
+from gptcache import Cache
+from langchain.globals import set_llm_cache
+from gptcache.manager.factory import manager_factory
+from gptcache.processor.pre import get_prompt
+from langchain_community.cache import GPTCache
+
+
+def get_hashed_name(name):
+    return hashlib.sha256(name.encode()).hexdigest()
+
+
+def init_gptcache(cache_obj: Cache, llm: str):
+    hashed_llm = get_hashed_name(llm)
+    cache_obj.init(
+        pre_embedding_func=get_prompt,
+        data_manager=manager_factory(manager="map", data_dir=f"map_cache_{hashed_llm}"),
+    )
+
+
+set_llm_cache(GPTCache(init_gptcache))
+
 class ProfessionalResponseGenerator:
     def __init__(self, model_name="mixtral-8x7b-32768"):
         """
